9c5493308d4ef86506105deb855e865982e574b7
Fernando González Aguilera <fgonzalezaguilera@gmail.com>
2018-04-18 09:39:29 +0200

    added test for getAll() method

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,34 @@
+const nock = require('nock');
 const expect = require('chai').expect;
 
-describe('First test', () => {
-  it('Should assert true to be true', () => {
-    expect(true).to.be.true;
+const restCountries = require('../index');
+
+describe('getAll()', () => {
+
+  let rc = new restCountries;
+  let getAllResponse = require('./mock/get-all-response');
+
+  beforeEach(() => {
+    nock('https://restcountries.eu')
+      .get('/rest/v2/all')
+      .reply(200, getAllResponse);
   });
-}); 
+
+  it('returns an object', () => {
+    return rc.getAll()
+      .then(response => {
+        expect(typeof response).to.equal('object');
+      });
+  });
+
+  it('returns three elements of type object inside the response', () => {
+    return rc.getAll()
+      .then(response => {
+        for (let value of response) {
+          expect(typeof value).to.equal('object');
+        };
+
+        expect(response.length).to.equal(3);
+      });
+  });
+});
---
+++ test/mock/get-all-response.js
@@ -0,0 +1,252 @@
+module.exports = [
+  {
+    "alpha2Code": "ES",
+    "alpha3Code": "ESP",
+    "altSpellings": [
+      "ES",
+      "Kingdom of Spain",
+      "Reino de España"
+    ],
+    "area": 505992.0,
+    "borders": [
+      "AND",
+      "FRA",
+      "GIB",
+      "PRT",
+      "MAR"
+    ],
+    "callingCodes": [
+      "34"
+    ],
+    "capital": "Madrid",
+    "cioc": "ESP",
+    "currencies": [
+      {
+        "code": "EUR",
+        "name": "Euro",
+        "symbol": "€"
+      }
+    ],
+    "demonym": "Spanish",
+    "flag": "https://restcountries.eu/data/esp.svg",
+    "gini": 34.7,
+    "languages": [
+      {
+        "iso639_1": "es",
+        "iso639_2": "spa",
+        "name": "Spanish",
+        "nativeName": "Español"
+      }
+    ],
+    "latlng": [
+      40.0,
+      -4.0
+    ],
+    "name": "Spain",
+    "nativeName": "España",
+    "numericCode": "724",
+    "population": 46438422,
+    "region": "Europe",
+    "regionalBlocs": [
+      {
+        "acronym": "EU",
+        "name": "European Union",
+        "otherAcronyms": [],
+        "otherNames": []
+      }
+    ],
+    "subregion": "Southern Europe",
+    "timezones": [
+      "UTC",
+      "UTC+01:00"
+    ],
+    "topLevelDomain": [
+      ".es"
+    ],
+    "translations": {
+      "br": "Espanha",
+      "de": "Spanien",
+      "es": "España",
+      "fa": "اسپانیا",
+      "fr": "Espagne",
+      "hr": "Španjolska",
+      "it": "Spagna",
+      "ja": "スペイン",
+      "nl": "Spanje",
+      "pt": "Espanha"
+    }
+  },
+  {
+    "alpha2Code": "DE",
+    "alpha3Code": "DEU",
+    "altSpellings": [
+      "DE",
+      "Federal Republic of Germany",
+      "Bundesrepublik Deutschland"
+    ],
+    "area": 357114.0,
+    "borders": [
+      "AUT",
+      "BEL",
+      "CZE",
+      "DNK",
+      "FRA",
+      "LUX",
+      "NLD",
+      "POL",
+      "CHE"
+    ],
+    "callingCodes": [
+      "49"
+    ],
+    "capital": "Berlin",
+    "cioc": "GER",
+    "currencies": [
+      {
+        "code": "EUR",
+        "name": "Euro",
+        "symbol": "€"
+      }
+    ],
+    "demonym": "German",
+    "flag": "https://restcountries.eu/data/deu.svg",
+    "gini": 28.3,
+    "languages": [
+      {
+        "iso639_1": "de",
+        "iso639_2": "deu",
+        "name": "German",
+        "nativeName": "Deutsch"
+      }
+    ],
+    "latlng": [
+      51.0,
+      9.0
+    ],
+    "name": "Germany",
+    "nativeName": "Deutschland",
+    "numericCode": "276",
+    "population": 81770900,
+    "region": "Europe",
+    "regionalBlocs": [
+      {
+        "acronym": "EU",
+        "name": "European Union",
+        "otherAcronyms": [],
+        "otherNames": []
+      }
+    ],
+    "subregion": "Western Europe",
+    "timezones": [
+      "UTC+01:00"
+    ],
+    "topLevelDomain": [
+      ".de"
+    ],
+    "translations": {
+      "br": "Alemanha",
+      "de": "Deutschland",
+      "es": "Alemania",
+      "fa": "آلمان",
+      "fr": "Allemagne",
+      "hr": "Njemačka",
+      "it": "Germania",
+      "ja": "ドイツ",
+      "nl": "Duitsland",
+      "pt": "Alemanha"
+    }
+  },
+  {
+    "alpha2Code": "CO",
+    "alpha3Code": "COL",
+    "altSpellings": [
+      "CO",
+      "Republic of Colombia",
+      "República de Colombia"
+    ],
+    "area": 1141748.0,
+    "borders": [
+      "BRA",
+      "ECU",
+      "PAN",
+      "PER",
+      "VEN"
+    ],
+    "callingCodes": [
+      "57"
+    ],
+    "capital": "Bogotá",
+    "cioc": "COL",
+    "currencies": [
+      {
+        "code": "COP",
+        "name": "Colombian peso",
+        "symbol": "$"
+      }
+    ],
+    "demonym": "Colombian",
+    "flag": "https://restcountries.eu/data/col.svg",
+    "gini": 55.9,
+    "languages": [
+      {
+        "iso639_1": "es",
+        "iso639_2": "spa",
+        "name": "Spanish",
+        "nativeName": "Español"
+      }
+    ],
+    "latlng": [
+      4.0,
+      -72.0
+    ],
+    "name": "Colombia",
+    "nativeName": "Colombia",
+    "numericCode": "170",
+    "population": 48759958,
+    "region": "Americas",
+    "regionalBlocs": [
+      {
+        "acronym": "PA",
+        "name": "Pacific Alliance",
+        "otherAcronyms": [],
+        "otherNames": [
+          "Alianza del Pacífico"
+        ]
+      },
+      {
+        "acronym": "USAN",
+        "name": "Union of South American Nations",
+        "otherAcronyms": [
+          "UNASUR",
+          "UNASUL",
+          "UZAN"
+        ],
+        "otherNames": [
+          "Unión de Naciones Suramericanas",
+          "União de Nações Sul-Americanas",
+          "Unie van Zuid-Amerikaanse Naties",
+          "South American Union"
+        ]
+      }
+    ],
+    "subregion": "South America",
+    "timezones": [
+      "UTC-05:00"
+    ],
+    "topLevelDomain": [
+      ".co"
+    ],
+    "translations": {
+      "br": "Colômbia",
+      "de": "Kolumbien",
+      "es": "Colombia",
+      "fa": "کلمبیا",
+      "fr": "Colombie",
+      "hr": "Kolumbija",
+      "it": "Colombia",
+      "ja": "コロンビア",
+      "nl": "Colombia",
+      "pt": "Colômbia"
+    }
+  }
+];
