f84617f464c0ba05204b2828a288d18b069a058a
Fernando González Aguilera <fgonzalezaguilera@gmail.com>
2018-04-20 16:35:35 +0200

    added findByName() method

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,12 @@
 const nock = require('nock');
 const expect = require('chai').expect;
 
-const restCountries = require('../index');
+const RestCountries = require('../index');
+
+let rc = new RestCountries;
 
 describe('getAll()', () => {
 
-  let rc = new restCountries;
   let getAllResponse = require('./mock/get-all-response');
 
   beforeEach(() => {
@@ -29,6 +30,32 @@ describe('getAll()', () => {
         };
 
         expect(response.length).to.equal(3);
+      });
+  });
+});
+
+describe('findByName()', () => {
+
+  let findByNameResponse = require('./mock/find-by-name-response');
+
+  beforeEach(() => {
+    nock('https://restcountries.eu')
+      .get('/rest/v2/name/france')
+      .reply(200, findByNameResponse);
+  });
+
+  it('returns an object', () => {
+    return rc.findByName('france')
+      .then(response => {
+        expect(typeof response).to.equal('object');
+      });
+  });
+
+  it('returns one element of type object inside the response', () => {
+    return rc.findByName('france')
+      .then(response => {
+        expect(typeof response[0]).to.equal('object');
+        expect(response.length).to.equal(1);
       });
   });
 });
---
+++ test/mock/find-by-name-response.js
@@ -0,0 +1,92 @@
+module.exports = [
+  {
+    "alpha2Code": "FR",
+    "alpha3Code": "FRA",
+    "altSpellings": [
+      "FR",
+      "French Republic",
+      "République française"
+    ],
+    "area": 640679.0,
+    "borders": [
+      "AND",
+      "BEL",
+      "DEU",
+      "ITA",
+      "LUX",
+      "MCO",
+      "ESP",
+      "CHE"
+    ],
+    "callingCodes": [
+      "33"
+    ],
+    "capital": "Paris",
+    "cioc": "FRA",
+    "currencies": [
+      {
+        "code": "EUR",
+        "name": "Euro",
+        "symbol": "€"
+      }
+    ],
+    "demonym": "French",
+    "flag": "https://restcountries.eu/data/fra.svg",
+    "gini": 32.7,
+    "languages": [
+      {
+        "iso639_1": "fr",
+        "iso639_2": "fra",
+        "name": "French",
+        "nativeName": "français"
+      }
+    ],
+    "latlng": [
+      46.0,
+      2.0
+    ],
+    "name": "France",
+    "nativeName": "France",
+    "numericCode": "250",
+    "population": 66710000,
+    "region": "Europe",
+    "regionalBlocs": [
+      {
+        "acronym": "EU",
+        "name": "European Union",
+        "otherAcronyms": [],
+        "otherNames": []
+      }
+    ],
+    "subregion": "Western Europe",
+    "timezones": [
+      "UTC-10:00",
+      "UTC-09:30",
+      "UTC-09:00",
+      "UTC-08:00",
+      "UTC-04:00",
+      "UTC-03:00",
+      "UTC+01:00",
+      "UTC+03:00",
+      "UTC+04:00",
+      "UTC+05:00",
+      "UTC+11:00",
+      "UTC+12:00"
+    ],
+    "topLevelDomain": [
+      ".fr"
+    ],
+    "translations": {
+      "br": "França",
+      "de": "Frankreich",
+      "es": "Francia",
+      "fa": "فرانسه",
+      "fr": "France",
+      "hr": "Francuska",
+      "it": "Francia",
+      "ja": "フランス",
+      "nl": "Frankrijk",
+      "pt": "França"
+    }
+  }
+];
